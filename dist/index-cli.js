0c759fbb326d6a54641b438c3333dee8b6875f25
Nick Wronski <nick@codeschool.com>
2015-11-07 01:39:50 -0500

    Display batch operation summary on process exit.

--- dist/index-cli.js
+++ dist/index-cli.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x6, _x7, _x8) { var _again = true; _function: while (_again) { var object = _x6, property = _x7, receiver = _x8; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x6 = parent; _x7 = property; _x8 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x10, _x11, _x12) { var _again = true; _function: while (_again) { var object = _x10, property = _x11, receiver = _x12; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x10 = parent; _x11 = property; _x12 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -58,7 +58,7 @@ var CliBatchTranscodeVideo = (function (_BatchTranscodeVideo) {
       var _this = this;
 
       process.on('exit', function () {
-        return _this.progress.finish();
+        return _this.finish();
       });
       this.progress.start();
 
@@ -81,10 +81,27 @@ var CliBatchTranscodeVideo = (function (_BatchTranscodeVideo) {
         clearInterval(this.timer);
         this.timer = null;
       }
+      this.progress.clear();
       this.progress.finish();
+      this.progress.summary(this.finalState());
       if (!this.isSuccess) {
         process.reallyExit(1);
       }
+    }
+  }, {
+    key: 'finalState',
+    value: function finalState() {
+      var processed = this.processedFileSizes;
+      var total = this.totalFileSizes;
+      // TODO: Not sure if this is working correctly
+      var speed = processed / (this.totalTime / 1000.0);
+      return {
+        files: this.files,
+        processed: processed.toFixed(2),
+        total: total.toFixed(2),
+        speed: speed.toFixed(2),
+        success: this.isSuccess
+      };
     }
   }, {
     key: 'state',
@@ -146,17 +163,111 @@ var Progress = (function () {
     _classCallCheck(this, Progress);
 
     this.charm = charm;
+    this.counts = {
+      skipped: 0,
+      written: 0,
+      errored: 0,
+      queued: 0
+    };
+    this.color = {
+      skipped: 'blue',
+      written: 'green',
+      errored: 'red',
+      queued: 'yellow'
+    };
+    this.bold = {
+      skipped: false,
+      written: false,
+      errored: true,
+      queued: true
+    };
   }
 
   _createClass(Progress, [{
     key: 'start',
     value: function start() {
-      this.charm.display('reset').write('Starting batch operation...\n\n');
+      this.charm.display('bright').foreground('cyan').write('Starting batch operation...\n\n');
     }
   }, {
     key: 'finish',
     value: function finish() {
-      this.charm.display('reset').write('\nFinished processing.\n\n');
+      this.charm.display('bright').foreground('cyan').write('Finished processing.\n\n');
+    }
+  }, {
+    key: 'summary',
+    value: function summary(state) {
+      try {
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = state.files[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var file = _step.value;
+
+            var type = undefined;
+            if (file.isWritten) {
+              type = 'written';
+            } else if (file.isErrored || file.isRunning) {
+              type = 'errored';
+            } else if (file.isReady) {
+              type = 'queued';
+            } else {
+              type = 'skipped';
+            }
+            this.counts[type] += 1;
+            this.bulletPoint('' + this.labelPad(type.toUpperCase() + ': ', 10) + this.truncateStr(file.fileName, 0.75, 10), this.color[type], this.bold[type]);
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator['return']) {
+              _iterator['return']();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
+
+        try {
+          for (var _iterator2 = Object.keys(this.counts)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var type = _step2.value;
+
+            var total = this.counts[type];
+            if (total > 0) {
+              this.bulletPoint(total + ' file' + (total !== 1 ? 's' : '') + ' ' + type + '.', this.color[type], this.bold[type]);
+            }
+          }
+        } catch (err) {
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+              _iterator2['return']();
+            }
+          } finally {
+            if (_didIteratorError2) {
+              throw _iteratorError2;
+            }
+          }
+        }
+
+        this.bulletPoint('Processed ' + state.processed + ' MB of ' + state.total + ' total MB across ' + state.files.length + ' file' + (state.files.length !== 1 ? 's' : '') + '.', 'cyan');
+        var finalMsg = state.success ? 'Finished without error.' : 'Finished with errors.';
+        this.bulletPoint(finalMsg, state.success ? 'green' : 'red', true);
+      } catch (e) {
+        console.log(e.message);
+        console.log(e.stack);
+      }
     }
   }, {
     key: 'write',
@@ -164,13 +275,18 @@ var Progress = (function () {
       var cur = state.current;
       var total = state.total;
       this.bar('Current', cur.percent);
-      this.truncatedLine('File', cur.file, 'Left: ' + (0, _libUtilJs.millisecondsToStr)(cur.remaining));
-      // this.statusLine({label: 'Current', elapsed: cur.elapsed, remaining: cur.remaining});
+      this.truncatedLine('File', cur.file, (0, _libUtilJs.millisecondsToStr)(cur.remaining));
       this.charm.write('\n');
       this.bar('Total', total.percent);
-      // this.statusLine({label: 'Total', elapsed: total.elapsed, remaining: total.remaining});
-      this.truncatedLine('Status', total.status, 'Left: ' + (0, _libUtilJs.millisecondsToStr)(total.remaining));
+      this.truncatedLine('Status', total.status, (0, _libUtilJs.millisecondsToStr)(total.remaining));
       this.charm.write('\n');
+    }
+  }, {
+    key: 'bulletPoint',
+    value: function bulletPoint(line, color) {
+      var bold = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+
+      this.charm.display(bold ? 'bright' : 'reset').foreground(color).write(' - ' + line + '\n');
     }
   }, {
     key: 'truncatedLine',
@@ -178,9 +294,18 @@ var Progress = (function () {
       var str = arguments.length <= 1 || arguments[1] === undefined ? '' : arguments[1];
       var extra = arguments.length <= 2 || arguments[2] === undefined ? '' : arguments[2];
       var size = arguments.length <= 3 || arguments[3] === undefined ? 0.5 : arguments[3];
+      var labelLen = arguments.length <= 4 || arguments[4] === undefined ? 8 : arguments[4];
 
       var firstPad = ' '.repeat(CliBatchTranscodeVideo.FIRST_TAB);
-      var labelLen = 8;
+      var formatted = this.truncateStr(str, size, labelLen);
+      this.charm.foreground('blue').write('' + firstPad + this.labelPad(label + ': ', labelLen)).display('reset').foreground('white').write('' + formatted).foreground('yellow').write('  ' + extra + '\n');
+    }
+  }, {
+    key: 'truncateStr',
+    value: function truncateStr(str) {
+      var size = arguments.length <= 1 || arguments[1] === undefined ? 0.5 : arguments[1];
+      var labelLen = arguments.length <= 2 || arguments[2] === undefined ? 8 : arguments[2];
+
       var maxLen = 100.0 * size;
       var maxWithoutLabel = maxLen - labelLen;
       var formatted = undefined;
@@ -193,7 +318,7 @@ var Progress = (function () {
         var padding = ' '.repeat(paddingSize);
         formatted = '' + str + padding;
       }
-      this.charm.display('reset').write('' + firstPad + this.labelPad(label + ': ', labelLen) + formatted + '  ' + extra + '\n');
+      return formatted;
     }
   }, {
     key: 'labelPad',
@@ -203,15 +328,6 @@ var Progress = (function () {
       return label.length < pad ? label + ' '.repeat(pad - label.length) : label;
     }
   }, {
-    key: 'statusLine',
-    value: function statusLine(_ref) {
-      var label = _ref.label;
-      var elapsed = _ref.elapsed;
-      var remaining = _ref.remaining;
-
-      this.charm.display('reset').write(this.labelPad(label + ': ', 12) + 'Elapsed: ' + (0, _libUtilJs.millisecondsToStr)(elapsed) + '\tRemaining: ' + (0, _libUtilJs.millisecondsToStr)(remaining) + '\n');
-    }
-  }, {
     key: 'bar',
     value: function bar(label, percent) {
       var size = arguments.length <= 2 || arguments[2] === undefined ? 0.5 : arguments[2];
@@ -219,7 +335,7 @@ var Progress = (function () {
       var printPercent = (0, _libUtilJs.fractionToPercent)(percent);
       var colored = Math.round(Number.parseFloat(printPercent) * size);
       var uncolored = 100 * size - colored;
-      this.charm.display('reset').write(this.labelPad(label + ': ', CliBatchTranscodeVideo.FIRST_TAB)).background('cyan').write(' '.repeat(colored)).display('reset').background('black').write(' '.repeat(uncolored)).display('reset').display('bright').write('  ' + printPercent + '%\n');
+      this.charm.display('bright').foreground('blue').write(this.labelPad(label + ': ', CliBatchTranscodeVideo.FIRST_TAB)).display('reset').background('cyan').write(' '.repeat(colored)).display('reset').background('black').write(' '.repeat(uncolored)).display('reset').display('bright').write('  ' + printPercent + '%\n');
     }
   }, {
     key: 'clear',
--- index-cli.js
+++ index-cli.js
@@ -14,7 +14,7 @@ export default class CliBatchTranscodeVideo extends BatchTranscodeVideo {
   }
 
   cli() {
-    process.on('exit', () => this.progress.finish());
+    process.on('exit', () => this.finish());
     this.progress.start();
 
     this.progress.write(this.state());
@@ -33,10 +33,26 @@ export default class CliBatchTranscodeVideo extends BatchTranscodeVideo {
       clearInterval(this.timer);
       this.timer = null;
     }
+    this.progress.clear();
     this.progress.finish();
+    this.progress.summary(this.finalState());
     if (!this.isSuccess) {
       process.reallyExit(1);
     }
+  }
+
+  finalState() {
+    let processed = this.processedFileSizes;
+    let total = this.totalFileSizes;
+    // TODO: Not sure if this is working correctly
+    let speed = processed / (this.totalTime / 1000.0);
+    return {
+      files: this.files,
+      processed: processed.toFixed(2),
+      total: total.toFixed(2),
+      speed: speed.toFixed(2),
+      success: this.isSuccess
+    };
   }
 
   state() {
@@ -87,36 +103,103 @@ export default class CliBatchTranscodeVideo extends BatchTranscodeVideo {
 class Progress {
   constructor(charm) {
     this.charm = charm;
+    this.counts = {
+      skipped: 0,
+      written: 0,
+      errored: 0,
+      queued: 0
+    };
+    this.color = {
+      skipped: 'blue',
+      written: 'green',
+      errored: 'red',
+      queued: 'yellow'
+    };
+    this.bold = {
+      skipped: false,
+      written: false,
+      errored: true,
+      queued: true
+    };
   }
 
   start() {
     this.charm
-    .display('reset')
+    .display('bright')
+    .foreground('cyan')
     .write('Starting batch operation...\n\n');
   }
 
   finish() {
     this.charm
-    .display('reset')
-    .write('\nFinished processing.\n\n');
+    .display('bright')
+    .foreground('cyan')
+    .write('Finished processing.\n\n');
+  }
+
+  summary(state) {
+    try {
+      for (let file of state.files) {
+        let type;
+        if (file.isWritten) {
+          type = 'written';
+        } else if (file.isErrored || file.isRunning) {
+          type = 'errored';
+        } else if (file.isReady) {
+          type = 'queued';
+        } else {
+          type = 'skipped'
+        }
+        this.counts[type] += 1;
+        this.bulletPoint(`${this.labelPad(`${type.toUpperCase()}: `, 10)}${this.truncateStr(file.fileName, 0.75, 10)}`, this.color[type], this.bold[type]);
+      }
+      for (let type of Object.keys(this.counts)) {
+        let total = this.counts[type];
+        if (total > 0) {
+          this.bulletPoint(`${total} file${total !== 1 ? 's' : ''} ${type}.`, this.color[type], this.bold[type]);
+        }
+      }
+      this.bulletPoint(`Processed ${state.processed} MB of ${state.total} total MB across ${state.files.length} file${state.files.length !== 1 ? 's' : ''}.`, 'cyan');
+      let finalMsg = state.success ?  `Finished without error.` :  `Finished with errors.`;
+      this.bulletPoint(finalMsg, (state.success ? 'green' : 'red'), true);
+    } catch (e) {
+      console.log(e.message);
+      console.log(e.stack);
+    }
   }
 
   write(state) {
     let cur = state.current;
     let total = state.total;
     this.bar(`Current`, cur.percent);
-    this.truncatedLine('File', cur.file, `Left: ${ms2Str(cur.remaining)}`);
-    // this.statusLine({label: 'Current', elapsed: cur.elapsed, remaining: cur.remaining});
+    this.truncatedLine('File', cur.file, ms2Str(cur.remaining));
     this.charm.write(`\n`);
     this.bar(`Total`, total.percent);
-    // this.statusLine({label: 'Total', elapsed: total.elapsed, remaining: total.remaining});
-    this.truncatedLine('Status', total.status, `Left: ${ms2Str(total.remaining)}`);
+    this.truncatedLine('Status', total.status, ms2Str(total.remaining));
     this.charm.write(`\n`);
   }
 
-  truncatedLine(label, str = '', extra = '', size = 0.5) {
+  bulletPoint(line, color, bold = false) {
+    this.charm
+    .display(bold ? 'bright' : 'reset')
+    .foreground(color)
+    .write(` - ${line}\n`);
+  }
+
+  truncatedLine(label, str = '', extra = '', size = 0.5, labelLen = 8) {
     let firstPad = ' '.repeat(CliBatchTranscodeVideo.FIRST_TAB);
-    let labelLen = 8;
+    let formatted = this.truncateStr(str, size, labelLen);
+    this.charm
+    .foreground('blue')
+    .write(`${firstPad}${this.labelPad(`${label}: `, labelLen)}`)
+    .display('reset')
+    .foreground('white')
+    .write(`${formatted}`)
+    .foreground('yellow')
+    .write(`  ${extra}\n`);
+  }
+
+  truncateStr(str, size = 0.5, labelLen = 8) {
     let maxLen = 100.0 * size;
     let maxWithoutLabel = maxLen - labelLen;
     let formatted;
@@ -129,19 +212,11 @@ class Progress {
       let padding = ' '.repeat(paddingSize);
       formatted = `${str}${padding}`;
     }
-    this.charm
-    .display('reset')
-    .write(`${firstPad}${this.labelPad(`${label}: `, labelLen)}${formatted}  ${extra}\n`);
+    return formatted;
   }
 
   labelPad(label, pad = 8) {
     return label.length < pad ? label + (' '.repeat(pad - label.length)) : label;
-  }
-
-  statusLine({label, elapsed, remaining}) {
-    this.charm
-    .display('reset')
-    .write(`${this.labelPad(`${label}: `, 12)}Elapsed: ${ms2Str(elapsed)}\tRemaining: ${ms2Str(remaining)}\n`);
   }
 
   bar(label, percent, size = 0.5) {
@@ -149,8 +224,10 @@ class Progress {
     let colored = Math.round(Number.parseFloat(printPercent) * size);
     let uncolored = (100 * size) - colored;
     this.charm
-    .display('reset')
+    .display('bright')
+    .foreground('blue')
     .write(this.labelPad(`${label}: `, CliBatchTranscodeVideo.FIRST_TAB))
+    .display('reset')
     .background('cyan')
     .write(' '.repeat(colored))
     .display('reset')
